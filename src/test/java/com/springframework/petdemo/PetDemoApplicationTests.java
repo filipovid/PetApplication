95a501f5f3e9ef13aaafed6ec01b853137c29075
filipovid <Whereismyrepo1>
2021-02-18 14:41:49 +0100

    Initial commit

---
+++ src/test/java/com/springframework/petdemo/PetDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.petdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
