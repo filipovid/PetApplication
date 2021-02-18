c365b70432751e0f3457f0375ee0940bf148e993
filipovid <Whereismyrepo1>
2021-02-18 14:46:51 +0100

    Adding POJOs. Closing Issue #1

---
+++ src/main/java/com/springframework/petdemo/model/Owner.java
@@ -0,0 +1,4 @@
+package com.springframework.petdemo.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/springframework/petdemo/model/Vet.java
@@ -0,0 +1,4 @@
+package com.springframework.petdemo.model;
+
+public class Vet extends Person {
+}
