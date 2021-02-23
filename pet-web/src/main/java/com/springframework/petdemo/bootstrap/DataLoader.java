260eacb26c05958019fc66d6547a980b4e815451
filipovid <Whereismyrepo1>
2021-02-23 15:40:01 +0100

    Adding PetTypes to Bootstrap. Closing Issue #32

--- pet-web/src/main/java/com/springframework/petdemo/bootstrap/DataLoader.java
+++ pet-web/src/main/java/com/springframework/petdemo/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.springframework.petdemo.bootstrap;
 
 import com.springframework.petdemo.model.Owner;
+import com.springframework.petdemo.model.PetType;
 import com.springframework.petdemo.model.Vet;
 import com.springframework.petdemo.services.OwnerService;
+import com.springframework.petdemo.services.PetTypeService;
 import com.springframework.petdemo.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,24 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
         Owner owner1 = new Owner();
         owner1.setFirstName("Micheal");
         owner1.setLastName("Weston");
