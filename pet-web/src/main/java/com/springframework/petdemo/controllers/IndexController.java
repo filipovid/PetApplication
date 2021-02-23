24d640d80f75f9f532107b9dce854a49e24f34f5
filipovid <Whereismyrepo1>
2021-02-23 16:46:37 +0100

    Fixing broken links. Closing Issue #45

--- pet-web/src/main/java/com/springframework/petdemo/controllers/IndexController.java
+++ pet-web/src/main/java/com/springframework/petdemo/controllers/IndexController.java
@@ -11,4 +11,9 @@ public class IndexController {
 
         return "index";
     }
+
+    @RequestMapping("/find")
+    public String oupsHandler() {
+        return "notimplemented";
+    }
 }
--- pet-web/src/main/java/com/springframework/petdemo/controllers/OwnerController.java
+++ pet-web/src/main/java/com/springframework/petdemo/controllers/OwnerController.java
@@ -21,4 +21,10 @@ public class OwnerController {
 
         return "owners/index";
     }
+
+    @RequestMapping("/find")
+    public String findOwner() {
+
+        return "notimplemented";
+    }
 }
